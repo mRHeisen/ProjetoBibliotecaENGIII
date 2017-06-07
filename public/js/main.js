4957ca2906e6f79b6d01f1f0bbef1b7475493b78
mRHeisen <miguel.avilla92@gmail.com>
2017-06-07 16:32:21 -0300

    Deploy Heroku

--- public/js/main.js
+++ public/js/main.js
@@ -2,32 +2,32 @@ angular.module('biblioteca', ['ngAnimate', 'ngRoute', 'ngResource', 'meusServico
 	.config(function($routeProvider, $locationProvider, $httpProvider) {
 
 		$routeProvider.when('/newobra', {
-			templateUrl: 'partials/formularios/CadastroObra.html',
+			templateUrl: 'partials/formularios/cadastroObra.html',
 			controller: 'ObraController'
 		});
 
 		$routeProvider.when('/obras', {
-			templateUrl: 'partials/views/ListaObra.html',
+			templateUrl: 'partials/views/listaObra.html',
 			controller: 'ListaObrasController'
 		});
 
 		$routeProvider.when('/obras/edit/:obraId', {
-			templateUrl: 'partials/formularios/CadastroObra.html',
+			templateUrl: 'partials/formularios/cadastroObra.html',
 			controller: 'ObraController'
 		});
 		
 		$routeProvider.when('/neweditora', {
-			templateUrl: 'partials/formularios/CadastroEditora.html',
+			templateUrl: 'partials/formularios/cadastroEditora.html',
 			controller: 'EditoraController'
 		});
 
 		$routeProvider.when('/newautor', {
-			templateUrl: 'partials/formularios/CadastroAutor.html',
+			templateUrl: 'partials/formularios/cadastroAutor.html',
 			controller: 'AutorController'
 		});
 
 		$routeProvider.when('/newgenero', {
-			templateUrl: 'partials/formularios/CadastroGenero.html',
+			templateUrl: 'partials/formularios/cadastroGenero.html',
 			controller: 'GeneroController'
 		});
 
